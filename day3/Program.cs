7b6a3bf304eb384203bdfe22342e5e8e16d4a718
Dennis Riis <dennis.riis@vend.com>
2025-12-03 10:32:13 +0100

    day3

---
+++ day3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Normally we will start by reading lines from an input file
+var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt");
+bool test = args.FirstOrDefault()?.StartsWith("test") ?? false;
+var sum = input.Select(bank => Joltage(bank,2)).Sum();
+WriteLine(sum);
+
+var sumPart2 = input.Select(bank => Joltage(bank, 12)).Sum();
+WriteLine(sumPart2);
+
+long Joltage(string bank, int length)
+{
+    var chars = JoltageRecur(bank, length);
+    return new string(chars).ToLong();
+}
+
+char[] JoltageRecur(string bank, int length)
+{
+    if (length == 0)
+        return [];
+    var nextCh = bank[..^(length-1)].Max();
+    var nextIndex = bank.IndexOf(nextCh) + 1;
+    var remaining = bank[nextIndex..];
+    char[] next = [nextCh];
+    return next.Concat(JoltageRecur(remaining, length - 1)).ToArray();
+}
